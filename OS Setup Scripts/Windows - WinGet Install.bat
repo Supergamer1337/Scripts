bd4e0f894ea915dedee2cf7da9e00adda75cfc7d
Supergamer1337 <felix.b.aronsson@gmail.com>
2021-06-25 09:08:18 +0200

    "Frequently Used" Programs section added

--- OS Setup Scripts/Windows - WinGet Install.bat
+++ OS Setup Scripts/Windows - WinGet Install.bat
@@ -1,3 +1,8 @@
+@cls
+@title Application Installer
+@echo off
+echo Installing essential packages.
+
 :: Get all essential software.
 winget install Brave
 winget install Notepad++
@@ -5,4 +10,19 @@ winget install VLC
 winget install Python
 winget install 7-Zip
 
-::winget install Discord
+:: Get "Frequently used" software
+cls
+echo Install frequently used programs (communication, gaming, entertainment, etc...)? (Y/N)
+set INPUT=
+set /P INPUT=%=%
+If /I "%INPUT%"=="y" goto yes
+If /I "%INPUT%"=="n" goto no
+
+:yes
+winget install Discord
+winget install QBittorrent
+winget install Spotify
+:: Get SpotMeDown setup here
+winget install Steam
+
+:no
